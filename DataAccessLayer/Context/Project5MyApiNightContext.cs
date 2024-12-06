358dfac4044b7d3825fe5fbb05687a581694ef99
Fatih Kürekçi <fatihkr02@gmail.com>
2024-12-06 22:12:00 +0300

    Add project files.

---
+++ DataAccessLayer/Context/Project5MyApiNightContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context
+{
+    public class Project5MyApiNightContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=FATIHKUREKCI\\SQLEXPRESS;initial Catalog=Project5MyApiNight;integrated Security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
